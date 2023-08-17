495e5a59fbf3d7a104b9d67442341093036dc0b1
Nordap64 <54553880+Nordap64@users.noreply.github.com>
2023-08-17 16:12:22 -0400

    Fixed errors regarding cursor lock

--- FishcordProject/Assets/Scripts/MainMenu Script/The Main Menu.cs
+++ FishcordProject/Assets/Scripts/MainMenu Script/The Main Menu.cs
@@ -8,5 +8,12 @@ public class ScenesManager : MonoBehaviour
 	public void MoveToPlay(int sceneID)
 	{
 		SceneManager.LoadScene(sceneID);
+
+		// I added these 3 lines to make sure the game always is unpaused when you hit play.
+		Time.timeScale = 1f;
+
+		// These are done automatically but I left them here for better readability
+		Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
 	}
 }
--- FishcordProject/Assets/Scripts/PauseMenu.cs
+++ FishcordProject/Assets/Scripts/PauseMenu.cs
@@ -24,22 +24,32 @@ public class PauseMenu : MonoBehaviour
         if (isPaused)
         {
             pauseMenu.SetActive(true);
+            //I added these 2 lines per statement.
+            // What the problem was before was the cursor was never unlocked. You were just seeing Unity unlock ur cursor from the game itself.
+            // Adding this allows the buttons to be pressed, and the functions to run.
+
+            //I also changed the organization of the scene slightly.
+            // It was pointless to have the Pausemenu AND the menu manager have the script.
+            // The manager should be the only thing containing the script, pointing to the pausemenu item.
+            // This way the script isnt called twice by mistake. I put the pausemenu item inside of the MenuManager item for organization purposes.
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
             Time.timeScale = 0f; // Pause the game by setting time scale to 0
         }
         else
         {
             pauseMenu.SetActive(false);
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
             Time.timeScale = 1f; // Resume the game by setting time scale back to 1
         }
-    }
-
-    public void Resume() //still working on this but the Main Menu one was the one i wanted to try and change the most
-    {
-        Time.timeScale = 1f;
     }
 
     public void MainMenu(int SceneID)
     {
         SceneManager.LoadScene(SceneID); // Load the main menu with build index 0 (maybe?)
+
+        //This is correct. But when you return from the mainmenu timescale is still set to 0.
+        // You would have to include this in the mainmenu script to make sure it is always set to 1 when you click Play
     }
 }
